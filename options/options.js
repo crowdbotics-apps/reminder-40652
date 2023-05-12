5d8f055b12400d92374e9292971dc5ef384f86ca
Crowdbotics <team@crowdbotics.com>
2023-05-12 14:31:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "reminder-40652",
+  url: "https://reminder-40652.botics.co",
+  api: "https://reminder-40652.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
